dedbebacdc189f7d4486f97b096a3aa0a1ca89a8
johnasharifi <johnasharifi@gmail.com>
2020-12-06 16:31:12 -0800

    Implement TextToPixelTable
    ReadFile will be able to provide a map<pair<int,int>, Pixel> structure.
    
    This is simply a mapping from pixel index i,j to the Pixel at those coords

--- BitmapToShaderExpression/ReadFile.cpp
+++ BitmapToShaderExpression/ReadFile.cpp
@@ -16,6 +16,26 @@ std::string ReadFile::ReadText(const::std::string& filepath)
 	return ss.str();
 }
 
+std::map<std::pair<int, int>, Pixel> ReadFile::TextToPixelTable(const::std::string& data) {
+	std::map<std::pair<int, int>, Pixel> pixelTable;
+	std::istringstream iss(data);
+	std::string line;
+
+	while (getline(iss, line)) {
+		int x, y;
+		float r, g, b;
+
+		// parse a line / string as a series of ints/floats/commas
+		sscanf_s(line.c_str(), "%d,%d,%f,%f,%f", &x, &y, &r, &g, &b);
+
+		Pixel px{ r, g, b };
+
+		pixelTable.emplace(std::pair<int,int>(x, y), px);
+	}
+
+	return pixelTable;
+}
+
 /// Converts a string into a collection of pixels and pixel counts. Similar to a freq table
 std::map<Pixel, int> ReadFile::TextToSparsePixels(const::std::string& data)
 {
