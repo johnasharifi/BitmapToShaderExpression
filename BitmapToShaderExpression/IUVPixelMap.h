f44afadf53282b7c1ff92f22f84a1d6f89e96cc2
johnasharifi <johnasharifi@gmail.com>
2020-12-12 21:56:44 -0800

    Document the IUVPixelMap's purpose
    The IUVPixelMap's purpose is to map from float,float to a Pixel

--- BitmapToShaderExpression/IUVPixelMap.h
+++ BitmapToShaderExpression/IUVPixelMap.h
@@ -2,11 +2,19 @@
 
 #include "ReadFile.h"
 
+/*
+An interface base class whose implementers will abstractly support getting a pixel when given a UV coordinate.
+*/
 class IUVPixelMap
 {
 public:
 	IUVPixelMap();
+	
+	/*
+	The IUVPixelMap will support getting a pixel when given a uv coordinate.
+	*/
 	virtual Pixel Get(float uvu, float uvv) = 0;
+
 	~IUVPixelMap();
 };
 
