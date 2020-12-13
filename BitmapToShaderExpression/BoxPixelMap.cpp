d31e5e9dc0559f7acccf1c8bb7f40fa55995566b
johnasharifi <johnasharifi@gmail.com>
2020-12-12 22:25:01 -0800

    Introduce the BoxPixelMap class
    BoxPixelMap implements IUVPixelMap and will give basic, slow functionality.

---
+++ BitmapToShaderExpression/BoxPixelMap.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include "BoxPixelMap.h"
+
+
+BoxPixelMap::BoxPixelMap()
+{
+}
+
+BoxPixelMap::~BoxPixelMap()
+{
+}
+
+Pixel Get(float uvu, float uvv) {
+	return Pixel{ 0,0,0 };
+}
---
+++ BitmapToShaderExpression/BoxPixelMap.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "IUVPixelMap.h"
+#include "ReadFile.h"
+
+/*
+A class for looking up pixel colors within certain bounds.
+*/
+class BoxPixelMap :
+	public IUVPixelMap
+{
+public:
+	BoxPixelMap();
+	~BoxPixelMap();
+};
+
